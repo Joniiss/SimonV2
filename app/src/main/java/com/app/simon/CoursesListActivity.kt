526375c3e4054eb12674c8ae2d81c9d13f9f110c
CarusoPUC <marcos.c.junior2@gmail.com>
2025-08-30 23:20:24 -0300

    Styling course list

--- app/src/main/java/com/app/simon/CoursesListActivity.kt
+++ app/src/main/java/com/app/simon/CoursesListActivity.kt
@@ -1,6 +1,8 @@
 package com.app.simon
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +13,19 @@ class CoursesListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_courses_list)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Clique no header
+        val header = findViewById<LinearLayout>(R.id.header)
+        header.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+            finish() // fecha a CoursesListActivity
         }
     }
 }
